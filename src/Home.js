2c9c1a0640fdf59e1f56248deac3cad976fe2b4f
Jack Lewis <jack.lewis159@gmail.com>
2023-01-17 18:43:52 +0000

    - Finalised basic navbar styling and palette - Made navbar mobile responsive with animated hamburger menu

--- src/Home.js
+++ src/Home.js
@@ -4,14 +4,20 @@ function Home() {
     return (
         <div className="home">
             <body>
-                <img alt="AI-generated stylised rendition of a portrait of the owner of this site,
-                framed on the neck up" src="./img/lensapic.jpg" />
-                <h1>
-                    Hey! I'm a computer science graduate and I'm looking to kickstart my career in software engineering.
-                </h1>
-                <h1>
-                    In a year's time, I'd love to be able to point at an exciting product and say: <h1>"We did that"</h1>.
-                </h1>
+                <ul className="dialogue">
+                    <li>
+                        <img className="lensa" alt="AI-generated stylised rendition of a portrait of the owner of this site,
+                        framed on the neck up" src="https://jacklewis.me/img/lensapic.jpg" />
+                        <h1>
+                            Hey! I'm a computer science graduate and I'm looking to kickstart my career in software engineering.
+                        </h1>
+                    </li>
+                    <li>
+                        <h1>
+                            In a year's time, I'd love to be able to point at an exciting product and say: "We did that"
+                        </h1>
+                    </li>
+                </ul>
             </body>
         </div>
     );
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,24 +1,29 @@
 import '../styles/NavBar.css';
+import {useState} from "react";
 
 function NavBar() {
+    //Initiating a hook to create state variable 'isOpen' and function 'setIsOpen' to update the state
+    const [isOpen, setIsOpen] = useState(false);
     return(
-        <nav className="navigation">
-            <a href="/" className="name">
-                Jack Lewis
-            </a>
-            <div className="navigation-menu">
-                <ul>
-                    <li>
-                        <a href="/home">Home</a>
-                    </li>
-                    <li>
-                        <a href="/about">About</a>
-                    </li>
-                    <li>
-                        <a href="/contact">Contact</a>
-                    </li>
-                </ul>
+        <nav className="nav-container">
+            <a href="/" className="nav-name">Jack Lewis</a>
+            <div className="nav-links">
+                <a href="/">Home</a>
+                <a href="/about">About</a>
+                <a href="/contact">Contact</a>
             </div>
+            <div className="nav-hamburger" onClick={() => setIsOpen(!isOpen)}>
+                <div className={`bar1 ${isOpen ? 'change' : ''}`}></div>
+                <div className={`bar2 ${isOpen ? 'change' : ''}`}></div>
+                <div className={`bar3 ${isOpen ? 'change' : ''}`}></div>
+            </div>
+            {isOpen && (
+                <div className="responsive-nav-links">
+                    <a href="/">Home</a>
+                    <a href="/about">About</a>
+                    <a href="/contact">Contact</a>
+                </div>
+            )}
         </nav>
     );
 }
