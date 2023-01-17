c9f4acb6aeec799a757abac94bb7b700ef00f565
Jack Lewis <jack.lewis159@gmail.com>
2023-01-17 00:22:45 +0000

    Added first couple of lines of text and an image to the home page

--- src/Home.js
+++ src/Home.js
@@ -1,13 +1,20 @@
 import './styles/Home.css';
 
 function Home() {
-  return (
-    <div className="App">
-      <body>
-
-      </body>
-    </div>
-  );
+    return (
+        <div className="home">
+            <body>
+                <img alt="AI-generated stylised rendition of a portrait of the owner of this site,
+                framed on the neck up" src="./img/lensapic.jpg" />
+                <h1>
+                    Hey! I'm a computer science graduate and I'm looking to kickstart my career in software engineering.
+                </h1>
+                <h1>
+                    In a year's time, I'd love to be able to point at an exciting product and say: <h1>"We did that"</h1>.
+                </h1>
+            </body>
+        </div>
+    );
 }
 
 export default Home;
