27a4950c5156db61a0570054c30782bc70ea760a
Jack Lewis <jack.lewis159@gmail.com>
2023-01-17 21:44:42 +0000

    - Added a game dialogue style with Pokemon font to the home page introduction - Changed the portrait image to a pixel art version

--- src/Home.js
+++ src/Home.js
@@ -6,15 +6,20 @@ function Home() {
             <body>
                 <ul className="dialogue">
                     <li>
-                        <img className="lensa" alt="AI-generated stylised rendition of a portrait of the owner of this site,
-                        framed on the neck up" src="https://jacklewis.me/img/lensapic.jpg" />
-                        <h1>
-                            Hey! I'm a computer science graduate and I'm looking to kickstart my career in software engineering.
-                        </h1>
+                        <img className="lensa" alt="AI-generated stylised portrait rendition of the owner of this site,
+                            framed from the neck up" src="https://jacklewis.me/img/lensapic.png" />
+                        <div className="message">
+                            <a href="/about" className="character">Jack</a>
+                            <div className="text">
+                                <h1>
+                                    Hi there. I'm a computer science graduate and I'm looking to kickstart my career in software engineering.
+                                </h1>
+                            </div>
+                        </div>
                     </li>
                     <li>
                         <h1>
-                            In a year's time, I'd love to be able to point at an exciting product and say: "We did that"
+                            In a year's time, I'd love to be able to point at an exciting product and say: "We did that".
                         </h1>
                     </li>
                 </ul>
