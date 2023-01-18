6f49ac7c5a54e732880a5e685c30e22f6a24b479
Jack Lewis <jack.lewis159@gmail.com>
2023-01-18 15:07:15 +0000

    - Added full name and tagline to About section - Added social media icons

--- src/Home.js
+++ src/Home.js
@@ -2,34 +2,41 @@ import './styles/Home.css';
 
 function Home() {
     return (
-            <body className="home">
-                <ul className="dialogue">
-                    <li className="message1">
-                        <img className="lensa" alt="AI-generated stylised portrait rendition of the owner of this site,
-                            framed from the neck up" src="https://jacklewis.me/img/lensapic.png" />
-                        <div className="message">
-                            <a href="/about" className="character">Jack</a>
-                            <div className="text">
-                                <h1 id="intro">Hi there. I'm a computer science graduate looking to kickstart my career in software engineering.</h1>
-                            </div>
+        <body className="home">
+            <ul className="dialogue">
+                <li className="message1">
+                    <img className="portrait" alt="AI-generated stylised portrait rendition of the owner of this site,
+                        framed from the neck up" src="https://jacklewis.me/img/lensapic.png" />
+                    <div>
+                        <a href="/about" className="character">Jack</a>
+                        <div className="text">
+                            <h1 id="intro">Hi there. I'm a computer science graduate looking to kickstart my career in software engineering.</h1>
                         </div>
-                    </li>
-                    <li className="message2">
-                        <h1>
-                            In a year's time, I'd like to be able to point at an exciting product and say...
-                        </h1>
-                    </li>
-                    <li className="message3">
-                        <h2>
-                            "We did that."
-                        </h2>
-                    </li>
-                </ul>
-                <div className="new-section">
-                    <h1>Hiring? I'd love to work with you. Here's everything you need to know about me:</h1>
+                    </div>
+                </li>
+                <li className="message2">
+                    <h1>
+                        In a year's time, I'd like to be able to point at an exciting product and say...
+                    </h1>
+                </li>
+                <li className="message3">
+                    <h2>
+                        "We did that."
+                    </h2>
+                </li>
+                <li className="message4">
+                    <h1>Hiring? I'd love to help your team build something amazing. Here's everything you need to know about me:</h1>
                     <br/>
-                </div>
-            </body>
+                </li>
+            </ul>
+            <hr className="break"></hr>
+            <h1 className="name">Jack Lewis</h1><h1>Computing for Business and Management Grad</h1>
+            <div className="icon-row">
+                <a href=""><img alt="Email icon" className="icon" src="https://jacklewis.me/icons/email.svg" /></a>
+                <a href="https://github.com/Jackerlus"><img alt="Github icon" className="icon" src="https://jacklewis.me/icons/github.svg" /></a>
+                <a href="https://linkedin.com/in/jacklewis9"><img alt="LinkedIn icon" className="icon" src="https://jacklewis.me/icons/linkedin.svg" /></a>
+            </div>
+        </body>
     );
 }
 
--- src/index.js
+++ src/index.js
@@ -2,20 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './styles/index.css';
 import Home from './Home';
-import reportWebVitals from './reportWebVitals';
 import NavBar from "./components/NavBar";
-import Resume from "./Resume";
+// import Resume from "./Resume";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
       <NavBar />
       <Home />
-      <Resume />
   </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
