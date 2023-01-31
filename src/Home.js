b4bc81306c8e4045ea71709ba2c97aba05c1e79f
Jack Lewis <jack.lewis159@gmail.com>
2023-01-31 01:35:00 +0000

    Started converting Sass files to SCSS
    Added graduation image to about me section, enforced 1:1 aspect ratio with aspect-ratio property and added appropriate styling
    
    Made employment details text more concise.

--- src/Home.js
+++ src/Home.js
@@ -30,12 +30,14 @@ function Home() {
                 </li>
             </ul>
             <hr className="break"></hr>
-            <section id="about">
+            <section className="about">
                 <h1 className="name">Jack Lewis</h1>
+                <img alt="The author, Jack, in his graduation gown and cap posing with his father on his graduation day."
+                     className="img-graduation" src="https://jacklewis.me/img/graduation.jpg" />
                 <h1>BSc, Computing for Business and Management (2.1)</h1>
                 <h2>University of Sussex, Class of 2022</h2>
                 <section className="icon-row">
-                    <a href=""><img alt="Email icon" className="icon" src="https://jacklewis.me/icons/email.svg" /></a>
+                    <a href="https://jacklewis.me"><img alt="Email icon" className="icon" src="https://jacklewis.me/icons/email.svg" /></a>
                     <a href="https://github.com/Jackerlus"><img alt="Github icon" className="icon" src="https://jacklewis.me/icons/github.svg" /></a>
                     <a href="https://linkedin.com/in/jacklewis9"><img alt="LinkedIn icon" className="icon" src="https://jacklewis.me/icons/linkedin.svg" /></a>
                 </section>
@@ -61,37 +63,33 @@ function Home() {
                         </ul>
                     </div>
                 </section>
-                <br />
                 <section className="employment">
                     <h1>Notable employment history:</h1>
                     <section className="employment-tesco">
                         <h2>Sales Assistant, Tesco Mobile (Current Role)</h2>
                         <ul>
-                            <li>Sales and customer service role selling mobile devices and network tariffs to customers</li>
+                            <li>Sales and customer service role selling mobile devices and network tariffs to customers,
+                                as well as performing technical troubleshooting.</li>
+                            <br/>
                             <li>This is a target-oriented role, and while having only just started I am pulling solid figures,
                                 including hitting targets for connections, colleague deals, ASP, marketing communications and more.</li>
+                            <br/>
                             <li>I create personal, welcoming experiences for customers coming into the store, whether they're buying
-                                anything or not, and go the extra mile in sorting out their issues regardless of if they contribute to my
-                                targets. Many of our customers are elderly and require that extra bit of patience and care in
+                                anything or not. Many of our customers are elderly and require that extra bit of patience and care in
                                 explaining concepts that may be alien to them.</li>
                         </ul>
                     </section>
                     <section className="employment-ussu">
                         <h2>IT Assistant, University of Sussex Students' Union</h2>
                         <ul>
-                            <li>Managing the IT systems of the students' union including sysadmin, network maintenance and expansion,
-                                helpdesk service and ordering new equipment. Large focus on virtualisation at first, then the department
-                                undertook a huge overhaul towards a ChromeOS-based setup company-wide.</li>
+                            <li>Managing the IT systems of the students' union including general sysadmin, network maintenance and expansion,
+                                helpdesk service, and ordering new equipment. Large focus on virtualisation at first, then the department
+                                undertook an overhaul towards a ChromeOS-based setup company-wide.</li>
                             <br />
-                            <li>Even though my position was as assistant, our IT manager was on long term sick leave for almost half of my
-                                time at the company, and so me and one other assistant were performing all aspects of managing the IT
-                                systems during this time including dealing with system outages of varying severity and making purchasing
-                                decisions for new technology and equipment with the support of upper management.</li>
-                            <br />
-                            <li>Technical expertise was of course an important part of this role, but half of my day to day job was using
-                                communication skills to explain and teach complex technical concepts to other employees that
-                                weren't as literate in IT terminology. Breaking down these into easily understood statements and actionable
-                                points was vital in my functions as an IT professional.</li>
+                            <li>Technical expertise was an important part of this role, but much of my day to day job was using
+                                communication skills to explain complex technical concepts to other employees that
+                                weren't as IT-literate. Being able to break these down into easily understood points was
+                                vital.</li>
                         </ul>
                     </section>
                 </section>
