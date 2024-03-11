5f1989857f3409238195094ad58eac3ad1413b8b
Jack Lewis <j.lewis@studioaka.co.uk>
2024-03-11 17:44:32 +0000

    Added BCT Stats to side projects section

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,11 +6,11 @@ function Home() {
             <ul className="dialogue">
                 <li className="message1">
                     <img className="portrait" alt="AI-generated stylised portrait rendition of the owner of this site,
-                        framed from the neck up" src="https://jacklewis.me/img/lensapic.png" />
+                        framed from the neck up" src="images/lensapic.png" />
                     <div>
-                        <a href="src" className="character">Jack</a>
+                        <a className="character">Jack</a>
                         <div className="text">
-                            <h1 id="intro">Hi there. I'm a computer science graduate looking to kickstart my career in software engineering.</h1>
+                            <h1 id="intro">I'm a computer science graduate looking to kickstart my career in software engineering.</h1>
                         </div>
                     </div>
                 </li>
@@ -34,14 +34,14 @@ function Home() {
                 <h1 className="name">Jack Lewis</h1>
                 <div className="img-container">
                     <img alt="The author, Jack, in his graduation gown and cap posing with his father on his graduation day."
-                        className="img-graduation" src="https://jacklewis.me/img/graduation.jpg" />
+                        className="img-graduation" src="images/graduation.jpg" />
                 </div>
                 <h1>BSc, Computing for Business and Management (2.1)</h1>
                 <h2>University of Sussex, Class of 2022</h2>
                 <div className="icon-row">
-                    <a href="https://jacklewis.me"><img alt="Email icon" className="icon" src="https://jacklewis.me/icons/email.svg" /></a>
-                    <a href="https://github.com/Jackerlus"><img alt="Github icon" className="icon" src="https://jacklewis.me/icons/github.svg" /></a>
-                    <a href="https://linkedin.com/in/jacklewis9"><img alt="LinkedIn icon" className="icon" src="https://jacklewis.me/icons/linkedin.svg" /></a>
+                    <a href="https://jacklewis.me"><img alt="Email icon" className="icon" src="images/icons/email.svg" /></a>
+                    <a href="https://github.com/Jackerlus"><img alt="Github icon" className="icon" src="images/icons/github.svg" /></a>
+                    <a href="https://linkedin.com/in/jacklewis9"><img alt="LinkedIn icon" className="icon" src="images/icons//linkedin.svg" /></a>
                 </div>
                 <section className="skills">
                     <div className="proficiency">
@@ -49,6 +49,7 @@ function Home() {
                         <ul>
                             <li>Java</li>
                             <li>HTML/CSS</li>
+                            <li>NodeJS development</li>
                             <li>JavaScript (incl. React and JQuery)</li>
                             <li>PHP</li>
                             <li>SQL</li>
@@ -87,13 +88,25 @@ function Home() {
                 <div className="project">
                     <a href="https://github.com/Jackerlus/jacklewis.me" className="project-link">
                         <img className="project-image" src="images/jacklewis.me.png"
-                            alt="A screenshot of the author's sounDB project. Features a screenshot of the top of the homepage of this website in the mobile display view." />
+                            alt="A screenshot of the top of the homepage of this website in the mobile display view." />
                     </a>
                     <h2>jacklewis.me (this site)</h2>
                     <ul className="project-description">
                         <li>My personal site. Used for my own dev experimentation and learning, as well as for showing my skills and projects to potential employers.</li>
-                        <li>Inspired by freeCodeCamp's OMDB movie searching app <a href='https://www.freecodecamp.org/news/how-to-build-a-movie-search-app-using-react-hooks-24eb72ddfaf7/'>tutorial</a>.</li>
                         <li>Through this project I learned valuable lessons in responsive design, accessibility, and React development.</li>
+                    </ul>
+                </div>
+                <br />
+                <div className="project">
+                    <a href="/" className="project-link">
+                        <img className="project-image" src="images/bctstats.png"
+                            alt="A screenshot of the author's gaming tournament statistics system front end. Shows inputs to search for players and matches, and assign teams." />
+                    </a>
+                    <h2>BCT Stats</h2>
+                    <ul className="project-description">
+                        <li>A web app based on the <a href="https://www.nodecg.dev/">nodecg</a> framework, co-developed with my friend <a href="https://github.com/baileybarberscargill">Bailey</a>. Designed to harvest statistics from VALORANT tournament matches via <a href="https://app.swaggerhub.com/apis-docs/Henrik-3/HenrikDev-API/3.0.0">Henrik's VALORANT API</a>.</li>
+                        <li>Stores statistics on players and matches in a database which can then be used to generate graphics directly to live broadcast on the fly.</li>
+                        <li>Developing BCT Stats taught me some great lessons in user experience, API usage, back-end development, and database interaction.</li>
                     </ul>
                 </div>
             </section>
